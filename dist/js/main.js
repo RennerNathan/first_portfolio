87e34f6e7c2e7c7c472b9210622af407705162c0
Nathan Renner <nathanrenner68@gmail.com>
2021-08-15 19:45:25 -0500

    Menu Button, javascript and rotate

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.menu_btn');
+const menu =  document.querySelector('.menu');
+const menuNav = document.querySelector('.menu_nav');
+const menuBranding = document.querySelector('.menu_branding');
+const navItems = document.querySelectorAll('.menu_nav_item');
+
+//Set Initial State of the Menu AKA (Open or False)//
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+    } else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = true;
+    }
+}
